d7175c5e9cfbb5a62f150883ac5044623ba799cb
ninelamericanel <ninelamericanel@gmail.com>
2022-01-20 11:27:31 +0300

    added services block

---
+++ slider.js
@@ -0,0 +1,11 @@
+new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  spaceBetween: 32,
+  // width: 240,
+  slidesPerView: 1.3,
+  slidesPerColumn: 1,
+});
